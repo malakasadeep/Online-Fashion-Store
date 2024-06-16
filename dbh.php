66265797b3c195cdd4457ca1e3dd73095a7d7491
malakasadeep <151869451+malakasadeep@users.noreply.github.com>
2024-06-16 20:46:30 +0530

    msg

--- dbh.php
+++ dbh.php
@@ -1,11 +1,11 @@
 <?php
-$serverName="localhost:3307";
-$dbUsername="admin2";
-$dbPassword="1234";
-$dbName="fashion";
+$serverName = "localhost:3307";
+$dbUsername = "admin2";
+$dbPassword = "1234";
+$dbName = "fashion";
 
-$conn=mysqli_connect($serverName,$dbUsername,$dbPassword,$dbName);
+$conn = mysqli_connect($serverName, $dbUsername, $dbPassword, $dbName);
 
-if(!$conn){
-    die("connection faild :".mysqli_connect_error());
+if (!$conn) {
+    die("connection unsucsess! :" . mysqli_connect_error());
 }
